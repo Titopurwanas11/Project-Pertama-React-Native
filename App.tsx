3d3c742607966900923649ed84ba5a5da74ee7b6
Titopurwanas11 <tito.purwana.sasmita@gmail.com>
2024-07-19 20:45:29 +0700

    doquote> merubah informasi default dari welcome react native

--- App.tsx
+++ App.tsx
@@ -77,20 +77,6 @@ function App(): React.JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Text> Hello word....</Text>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
         </View>
       </ScrollView>
     </SafeAreaView>
